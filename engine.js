8ac8aa1157e09005124754f49b26a5994b8bd0f6
Christopher Haster <chaster@utexas.edu>
2013-04-15 15:32:27 -0500

    Attempted a selection solution using hermite curves

--- engine.js
+++ engine.js
@@ -3,18 +3,31 @@ var mothership
 var lasttime
 var field
 
+var planets
 
-function init() {
-    mothership = ship('Terran', 40, 40, 10, 100, 50)
+var delay
+
+
+function init(d) {
+    mothership = ship('Terran', 40, 40, 20, 300, 50)
     lasttime = new Date().getTime()
     field = canvas('field')
+
+    delay = d ? d : 1
+
+    document.getElementById('field').onclick = function clickhandle(ev) {
+        if (!ev.ctrlKey)
+            mothership.bpoint(vec(ev.clientX, ev.clientY), vec(mothership.m_speed, 0))
+        else
+            mothership.waypoint(vec(ev.clientX, ev.clientY))
+    }
 
     run()
 }
 
 function run() {
     var nowtime = new Date().getTime()
-    var dt = (nowtime - lasttime)/1000
+    var dt = (nowtime - lasttime)/(1000 * delay)
 
     var ctx = canvas('field')
     ctx.clearRect(0, 0, 1000, 1000)
@@ -22,15 +35,15 @@ function run() {
 
     mothership.step(dt, field)
 
+    mothership.prestep(10)
+    mothership.select(field)
+
     lasttime = nowtime
-    setTimeout(run, 16)
+    setTimeout(run, 28 * delay)
 }
 
 
 function ship(race, x, y, mass, force, speed, hx, hy) {
-    if (race == undefined) 
-        race = 'Terran'
-
     race = 'data/races/' + race + '/' +
            race + '_Mini_Carrier.bmp'
 
@@ -54,7 +67,7 @@ function ship(race, x, y, mass, force, speed, hx, hy) {
         radsq: radsq,
 
         img:   image(race),
-        tasks: [],
+        tasks: []
     }
 
 
@@ -67,42 +80,114 @@ function ship(race, x, y, mass, force, speed, hx, hy) {
     }
 
     obj.select = function(ctx) {
+        if (!this.path)
+            return
+
+        ctx.beginPath()
+        ctx.strokeStyle = 'white'
+        ctx.moveTo(this.pos.x, this.pos.y)
+
+        for (var i=0; i<this.path.length; i++) {
+            ctx.lineTo(this.path[i].pos.x, this.path[i].pos.y)
+
+            old = this.path[i]
+        }
+
+        ctx.stroke()
+        ctx.closePath()
+    }
+
+    obj.prestep = function(time) {
+        this.path = []
+
+        var old = {
+            pos: this.pos,
+            vel: this.vel,
+            head: this.head,
+            tasks: this.tasks.slice(0)
+        }
+
+        for (var i=0; i<time*(1000/28); i++) {
+            this.step(28/(1000*delay))
+            this.path.push({
+                pos: this.pos,
+                vel: this.vel,
+                head: this.head,
+            })
+        }
+
+        this.pos = old.pos
+        this.vel = old.vel
+        this.head = old.head        
+        this.tasks = old.tasks
+    }
+
+    obj.poststep = function() {
 
     }
 
     obj.step = function(dt, debug) {
-        if (this.tasks.length < 1)
-            return
-
         if (debug) {
             debug.save()
             debug.translate(this.pos.x, this.pos.y)
         }
 
-        var dest = this.tasks[0](this, debug)
+        // Find tasks steering force
+        var dir = vec(0,0)
 
-        if (dest.dir.lensq() > this.m_force*this.m_force)
-            dest.dir = dest.dir.norm().scale(this.m_force)
+        if (this.tasks.length >= 1) {
+            if (this.tasks[0].done(this, dt, debug))
+                this.tasks.splice(0, 1)
+            else
+                dir = this.tasks[0].dir(this, dt, debug)
+        }
+
+
+        // cap at max force
+        if (dir.lensq() > this.m_force*this.m_force)
+            dir = dir.norm().scale(this.m_force)
+
 
         if (debug) {
             debug.beginPath()
             debug.strokeStyle = 'red'
             debug.moveTo(0,0)
-            debug.lineTo(dest.dir.x/this.m_force*this.rad, dest.dir.y/this.m_force*this.rad)
+            debug.lineTo(dir.x/this.m_force*this.rad, dir.y/this.m_force*this.rad)
             debug.stroke()
             debug.closePath()
         }
 
-        this.vel = this.vel.add(dest.dir.scale(dt/this.mass))
-        this.head = this.vel.norm()
 
+        // find new theoretical velocity
+        dir = this.vel.add(dir.scale(dt/this.mass))
+        
+        // cap angular velocity
+        var m_spin = (this.m_speed / this.rad) * dt
+
+        if (angle(dir, this.head) > m_spin) {
+            this.head = this.head.rotate(m_spin)
+            this.vel = this.head.scale(dot(dir, this.head))
+
+        } else if (angle(dir, this.head) < -m_spin) {
+            this.head = this.head.rotate(-m_spin)
+            this.vel = this.head.scale(dot(dir, this.head))
+
+        } else if (this.vel.lensq() != 0) {
+            this.head = dir.norm()
+            this.vel = dir 
+
+        } else {
+            this.vel = dir 
+        }
+
+
+        // cap linear velocity
         if (this.vel.lensq() > this.m_speed*this.m_speed)
             this.vel = this.vel.norm().scale(this.m_speed)
 
-        this.pos = this.pos.add(this.vel.scale(dt))
 
-        if (dest.done)
-            this.tasks.splice(0, 1)
+        // update position
+        this.pos = this.pos.add(dir.scale(dt))
 
         if (debug) {
             debug.beginPath()
@@ -123,22 +208,115 @@ function ship(race, x, y, mass, force, speed, hx, hy) {
     }
 
     obj.waypoint = function(targ) {
-        this.tasks.push(function(sh, debug) {
-            var dir = targ.sub(sh.pos)
-            var done = (dir.lensq() < sh.radsq)
+        this.tasks.push({
+            dir: function(sh, dt, debug) {
+                var dir = targ.sub(sh.pos)
 
-            if (debug) {
-                debug.beginPath()
-                debug.strokeStyle="purple"
-                debug.arc(dir.x, dir.y, 10, 0, 2*Math.PI)
-                debug.stroke()
-                debug.closePath()
+                if (debug) {
+                    debug.beginPath()
+                    debug.strokeStyle="purple"
+                    debug.arc(dir.x, dir.y, 8, 0, 2*Math.PI)
+                    debug.stroke()
+                    debug.closePath()
+                }
+
+                dir = dir.norm().scale(sh.m_speed)
+                dir = dir.sub(sh.vel).scale(sh.mass / dt)
+
+                return dir
+            },
+
+            done: function(sh, dt, debug) {
+                return targ.sub(sh.pos).lensq() < 5
             }
+        })
+    }
 
-            dir = dir.norm().scale(sh.m_speed)
-            dir = dir.sub(sh.vel).scale(sh.mass*sh.m_force/sh.m_speed)
+    obj.endpoint = function(targ) {
+        this.tasks.push({
+            stopdist: (this.m_speed*this.m_speed*this.mass) / (2*this.m_force),
 
-            return { dir: dir, done: done }
+            dir: function(sh, dt, debug) {
+                var dir = targ.sub(sh.pos)
+
+                if (debug) {
+                    debug.beginPath()
+                    debug.strokeStyle="purple"
+                    debug.arc(dir.x, dir.y, 8, 0, 2*Math.PI)
+                    debug.stroke()
+                    debug.closePath()
+
+                    debug.beginPath()
+                    debug.strokeStyle="white"
+                    debug.arc(dir.x, dir.y, this.stopdist, 0, 2*Math.PI)
+                    debug.stroke()
+                    debug.closePath()
+                }
+
+                dir = dir.norm().scale(sh.m_speed)
+
+                if (distsq(sh.pos, targ) < this.stopdist*this.stopdist) {
+                    dir = dir.scale(dist(sh.pos, targ) / this.stopdist)
+                }
+
+                dir = dir.sub(sh.vel).scale(sh.mass / dt)
+
+                return dir
+            },
+
+            done: function(sh, dt, debug) {
+                return (targ.sub(sh.pos).lensq() < 5) && (sh.vel.lensq() < 1)
+            }
+        })
+    }
+
+    obj.bpoint = function(targ, tvel) {
+        this.tasks.push({
+            dir: function(sh, dt, debug) {
+                if (debug) {
+                    var dir = targ.sub(sh.pos)
+
+                    debug.beginPath()
+                    debug.strokeStyle="purple"
+                    debug.arc(dir.x, dir.y, 8, 0, 2*Math.PI)
+                    debug.stroke()
+                    debug.closePath()
+
+                    debug.beginPath()
+                    debug.strokeStyle="magenta"
+                    debug.moveTo(0,0)
+                    debug.bezierCurveTo(sh.vel.x*3, sh.vel.y*3,
+                                        dir.x-tvel.x*3, dir.y-tvel.y*3,
+                                        dir.x, dir.y)
+                    debug.stroke()
+                    debug.closePath()
+                }
+
+                var dir = hermitep(sh.pos, sh.vel, targ, tvel, 0).scale(dt*dt).add( 
+                          hermitepp(sh.pos, sh.vel, targ, tvel, 0).scale(dt*dt/2))
+
+                dir = hermite_dist(sh.pos, sh.vel, targ, tvel, dir.length())
+
+                if (debug) {
+                    debug.beginPath()
+                    debug.strokeStyle="purple"
+                    debug.arc(hermite(sh.pos, sh.vel, targ, tvel, dir).sub(sh.pos).x, 
+                              hermite(sh.pos, sh.vel, targ, tvel, dir).sub(sh.pos).y, 
+                              8, 0, 2*Math.PI)
+                    debug.stroke()
+                    debug.closePath()
+                }
+
+                dir = hermitep(sh.pos, sh.vel, targ, tvel, dir)
+
+                dir = dir.sub(sh.vel).scale(sh.mass / dt)
+
+                return dir
+            },
+
+            done: function(sh, dt, debug) {
+                return (targ.sub(sh.pos).lensq() < 5)
+            }
         })
     }
 
--- vecmath.js
+++ vecmath.js
@@ -52,12 +52,19 @@ function vec(x,y) {
         return vec(this.x/l, this.y/l)
     }
 
+    obj.rotate = function(ang) {
+        var cs = Math.cos(ang)
+        var sn = Math.sin(ang)
+
+        return vec(x*cs - y*sn, x*sn + y*cs)
+    }
+
     return obj
 }
 
 function distsq(vec0, vec1) {
-    var x = (vec1.x-vec0.y)
-    var y = (vec1.x-vec0.y)
+    var x = (vec1.x-vec0.x)
+    var y = (vec1.y-vec0.y)
     return x*x + y*y
 }
 
@@ -73,6 +80,63 @@ function angle(vec0, vec1) {
     return -Math.atan2(-vec0.y*vec1.x + vec0.x*vec1.y, dot(vec0, vec1))
 }
 
-function interpolate(vec0, vec1, ratio) {
+function lerp(vec0, vec1, ratio) {
     return vec0.scale(1-ratio).add(vec1.scale(ratio))
+}
+
+function bezier(p0, p1, p2, p3, t) {
+    var mid = lerp(p1,p2,t)
+
+    return lerp(
+        lerp(lerp(p0,p1,t), mid, t),
+        lerp(mid, lerp(p2,p3,t), t), t
+    )
+}
+
+function hermite(p0, m0, p1, m1, t) {
+    return (
+        p0.scale(2*t*t*t - 3*t*t + 1).add(
+        m0.scale(t*t*t - 2*t*t + t).add(
+        p1.scale(-2*t*t*t + 3*t*t).add(
+        m1.scale(t*t*t - t*t)
+    ))))
+}
+
+function hermitep(p0, m0, p1, m1, t) {
+    return (
+        p0.scale(6*t*t - 6*t).add(
+        m0.scale(3*t*t - 4*t + 1).add(
+        p1.scale(-6*t*t + 6*t).add(
+        m1.scale(3*t*t - 2*t)
+    ))))
+}
+
+function hermitepp(p0, m0, p1, m1, t) {
+    return (
+        p0.scale(12*t - 6).add(
+        m0.scale(6*t - 4).add(
+        p1.scale(-12*t + 6).add(
+        m1.scale(6*t - 2)
+    ))))
+}
+
+function hermite_dist(p0, m0, p1, m1, dist, res) {
+    if (!res) 
+        res = 0.0001
+
+    var t0 = 0
+    var t1 = 1
+    var mid
+
+    do {
+        var mid = (t1-t0)/2 + t0
+        var actual = hermite(p0, m0, p1, m1, mid)
+
+        if (actual.lensq() < dist*dist)
+            t0 = mid
+        else
+            t1 = mid
+    } while (t1 - t0 > res);
+
+    return mid
 }
